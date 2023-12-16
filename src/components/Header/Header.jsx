e4195c9dfc54869af7d6406088e57f1db21afb0c
Omar Faruk <omarfaruk65142@gmail.com>
2023-12-16 10:35:54 +0600

    Complete the header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import Logo from '../../images/Logo.svg'
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className="header">
+            <img src={Logo} alt="" />
+            <div>
+                <a href="/Order">Order</a>
+                <a href="/Order Review">Order Review</a>
+                <a href="/Manage Inventory">Manage Inventory</a>
+                <a href="/Login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
