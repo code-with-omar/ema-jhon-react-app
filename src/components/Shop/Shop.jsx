9d7aef6f57c87428a5ace6ad8c6723ad08838d73
Omar Faruk <omarfaruk65142@gmail.com>
2023-12-20 13:10:57 +0600

    Complete set data in local store and aslo complete fetch the local store data and show it in ui

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,7 +3,7 @@ import './Shop.css'
 import { useEffect } from 'react';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { addToDb } from '../../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([])
@@ -13,11 +13,30 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+    //Add product in local store
     const handleAddToCart = (product) => {
         const newAddCart = [...cart, product];
         setCart(newAddCart);
         addToDb(product.id)
     }
+    // fetch save products data in local store here
+
+    useEffect(() => {
+        const storedData = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id
+        // console.log(storedData)
+        for (const id in storedData) {
+            const savedProduct = products.find(product => product.id === id);
+            const quantity = storedData[id];
+            if (savedProduct) {
+                savedProduct.quantity = quantity;
+                savedCart.push(savedProduct);
+            }
+        }
+
+        setCart(savedCart);
+    }, [products])
     return (
         <div className='shop-container'>
             <div className='products-container'>
