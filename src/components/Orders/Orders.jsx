5bc42ff1ac9d862ae057bb2ef5f7e75ef1cec002
Omar Faruk <omarfaruk65142@gmail.com>
2023-12-28 12:27:25 +0600

    Add removie iteam functionality

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -2,18 +2,29 @@ import { useLoaderData } from "react-router-dom";
 import Cart from "../Cart/Cart";
 import './Orders.css'
 import ReviewItem from "../ReviewIteam/ReviewItem";
+import { useState } from "react";
+import { removeFromDb } from "../../../utilities/fakedb";
 
 const Orders = () => {
-    const carts=useLoaderData()
+    const saveCarts = useLoaderData()
+    const [carts, setCart] = useState(saveCarts);
+    const handleReviewItemRemove = (id) => {
+        const reaminigIteam = carts.filter(product => product.id !== id);
+        removeFromDb(id)
+        setCart(reaminigIteam);
+        console.log('hello');
+    }
     return (
         <div className='shop-container'>
             <div className='review-container'>
                 {
-                   carts.map(cart=><ReviewItem key={cart.id} product={cart}></ReviewItem>) 
+                    carts
+                        .map(cart => <ReviewItem key={cart.id} product={cart} handleReviewItemRemove={handleReviewItemRemove}></ReviewItem>)
                 }
             </div>
             <div className='cart-container'>
-                <Cart cart={carts}></Cart>
+                <Cart cart={carts
+                }></Cart>
 
             </div>
         </div>
--- src/components/ReviewIteam/ReviewItem.jsx
+++ src/components/ReviewIteam/ReviewItem.jsx
@@ -1,18 +1,19 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 import './ReviewItem.css'
-const ReviewItem = ({product}) => {
-    const {img,id,price,name,quantity}=product
+const ReviewItem = ({ product ,handleReviewItemRemove}) => {
+    const { img, id, price, name, quantity } = product
     return (
         <div className='review-iteam'>
-           <img src={img} alt="" />
-           <div className="review-details">
+            <img src={img} alt="" />
+            <div className="review-details">
                 <p className='review-title'>{name}</p>
                 <p>Price : <span className='orange'>${price}</span></p>
                 <p>Order Quantity :<span className='orange'>{quantity}</span> </p>
-           </div>
-           <div>
-                <button className='delete-btn'>D</button>
-           </div>
+            </div>
+            <div className='delete-btn-wrap'>
+                <button className='delete-btn'><FontAwesomeIcon icon={faTrashAlt} onClick={()=>handleReviewItemRemove(id)}/></button>
+            </div>
         </div>
     );
 };
