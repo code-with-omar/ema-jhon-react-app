1d693009032eab193962c17560a101a65e3b8e8c
Omar Faruk <omarfaruk65142@gmail.com>
2024-01-08 23:19:53 +0600

    Create login components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,23 @@
-
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            This is login page
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="email" id="email" required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="password" id="password" required />
+                </div>
+                <div className='submit'>
+                    <label htmlFor=""></label>
+                    <input type="submit" value="Login" />
+                </div>
+            </form>
         </div>
     );
 };
