601f4c40cf7471a1311e4e3acad967917186755a
Omar Faruk <omarfaruk65142@gmail.com>
2024-01-08 23:59:56 +0600

    create password validation

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import './SignUp.css'
 
 const SignUp = () => {
+    const [error, setError] = useState('')
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const comfirmPassword = form.comfirm.value;
+        if (password !== comfirmPassword) {
+            setError("Password did not match");
+            return;
+        } else if (password < 6) {
+            setError("Password must grater then or equal six digits");
+        }
+    }
     return (
         <div className='form-container'>
             <h2 className='form-title'>Sign Up</h2>
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <div className='form-control'>
                     <label htmlFor="">Email</label>
                     <input type="email" name="email" id="email" required />
@@ -15,7 +30,10 @@ const SignUp = () => {
                 </div>
                 <div className='form-control'>
                     <label htmlFor="">Confirm Password</label>
-                    <input type="password" name="password" id="comfirm-password" required />
+                    <input type="password" name="comfirm" id="comfirm-password" required />
+                </div>
+                <div>
+                    <p>{error}</p>
                 </div>
                 <div className='submit'>
                     <label htmlFor=""></label>
