27f417281c185703bea2cd190d0d939488a5176e
Omar Faruk <omarfaruk65142@gmail.com>
2024-01-09 11:54:11 +0600

    Implement hold the current user and show in header section. And aslo implement when user login header show logout button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../images/Logo.svg'
 import './Header.css'
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+    const handleLogOut = () => {
+        logOut()
+            .then(() => {
+                console.log('Logout !')
+            }).catch(error => console.error(error));
+    }
     return (
         <nav className="header">
             <img src={Logo} alt="" />
@@ -11,6 +20,7 @@ const Header = () => {
                 <Link to="/inventory">Manage Inventory</Link>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Sign Up</Link>
+                {user && <><span className='user-login'>{user.email}</span> <Link onClick={handleLogOut}>Logout</Link></>}
             </div>
         </nav>
     );
--- src/components/providers/AuthProvider.jsx
+++ src/components/providers/AuthProvider.jsx
@@ -1,14 +1,12 @@
-import { createContext } from "react";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 //create auth
 const auth = getAuth(app);
 export const AuthContext = createContext(null);
-const AuthProvider = ({ children }) => {
 
-    const user = {
-        displayName: "Omar Faruk",
-    }
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
     //create sign up user
     const signUp = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
@@ -21,6 +19,13 @@ const AuthProvider = ({ children }) => {
     const logOut = () => {
         signOut(auth);
     }
+    //Get the currently signed-in user
+    const unsubscribe =onAuthStateChanged(auth, currentUser => {
+        setUser(currentUser);
+        return ()=>{
+            return unsubscribe()
+        }
+    })
     const authInfo = {
         user,
         signUp,
